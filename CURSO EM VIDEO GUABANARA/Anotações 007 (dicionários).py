aff0b7a3b4e58ac0782bd1a219acf2826dc22d2a
wagnerbmoura <wagnerbmoura@gmail.com>
2024-01-02 12:11:55 -0300

    PASTA SI

--- CURSO EM VIDEO GUABANARA/Anotações 007 (dicionários).py
+++ CURSO EM VIDEO GUABANARA/Anotações 007 (dicionários).py
@@ -1,2 +1,10 @@
+### Anotações avulsas ###
+
 casa = {'sala':['sofá','televisão','mesa'],'cozinha':['armário','filtro','fogão'],'quarto':['cama','guarda-roupa','sexto']}
 
+
+
+for k,v in casa.items():
+    print(f'O {k} é {v}')
+#=========================================================================================================#
+### Parte Prática ###
