732d589a6ed47745d019cacb39365d9527c8eb1c
wagnerbmoura <wagnerbmoura@gmail.com>
2024-01-04 14:01:06 -0300

    PASTA SI

--- CURSO EM VIDEO GUABANARA/COPIA TESTE.py
+++ CURSO EM VIDEO GUABANARA/COPIA TESTE.py
@@ -1,2 +1,8 @@
 Anotações 007 (dicionários).py
 SSSSSS
+Anotações 007 (dicionários).py
+SSSSSSAnotações 007 (dicionários).py
+SSSSSSAnotações 007 (dicionários).py
+SSSSSSAnotações 007 (dicionários).py
+SSSSSSAnotações 007 (dicionários).py
+SSSSSS
